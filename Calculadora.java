95766700b2fe719c6d2f5ce58925a239087344d9
Beatriz <beatrizcaroline.com.br@gmail.com>
2024-03-26 21:30:54 -0300

    Vs Professor

--- Calculadora.java
+++ Calculadora.java
@@ -1,6 +1,15 @@
-public class Calculadora {
+import javax.swing.JOptionPane;
 
-    double somar(double a, double b) {
-        return a + b
+public class CalculadoraTeste {
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora(
+                Double.parseDouble(JOptionPane.showInputDialog("Digite seu número preferido")));
+        double resultadoSoma = calc.somar(2, 2);
+        System.out.println(resultadoSoma);
+
+        System.out.println(calc.somarComPreferido(
+                Double.parseDouble(JOptionPane.showInputDialog("Digite seu número para somar com o preferido"))));
+
+        System.out.println(calc.subtrair(10, 3));
     }
 }
